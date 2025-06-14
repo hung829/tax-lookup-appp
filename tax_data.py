df67594ba7536c829057feae3536353ddf8ca3c7
hung829 <minhhunggg1706@gmail.com>
2025-06-14 13:20:42 +0700

    Update tax_data.py

--- tax_data.py
+++ tax_data.py
@@ -1,1 +1,14 @@
+# tax_data.py
+tax_records = {
+    "123456789": {
+        "Tên công ty": "Công ty TNHH ABC",
+        "Địa chỉ": "123 Đường A, TP.HCM",
+        "Ngành nghề": "Thương mại"
+    },
+    "987654321": {
+        "Tên công ty": "Công ty Cổ phần XYZ",
+        "Địa chỉ": "456 Đường B, Hà Nội",
+        "Ngành nghề": "Dịch vụ"
+    }
+}
 
